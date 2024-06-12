a2aac6ba9c3c2af4c8dc04e768642cdc1f2daf27
Emerson Queiroz <equeiroz.sp@gmail.com>
2024-06-11 22:12:02 -0300

    Moving to ejs template

--- backend/app.js
+++ backend/app.js
@@ -6,20 +6,48 @@ const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 
+
 var clientesRouter = require('./routes/clientes');
 var funcionariosRouter = require('./routes/funcionarios');
 var ingressosRouter = require('./routes/ingressos');
 var pedidosRouter = require('./routes/pedidos');
 
 var app = express();
+app.set('view engine', 'ejs')
+app.set('views', './views')
 app.use(cors({origin: process.env.CLIENT_ORIGIN_URL}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname, '..', 'assets'))
+app.use('/assets', express.static(path.join(__dirname, '..', 'assets')));
 
-// app.use('/', indexRouter);
+app.get('/', (req, res) => {
+  res.render("index")
+});
+
+app.get('/contatos', (req, res) => {
+  res.render("contatos")
+});
+
+app.get('/galeria', (req, res) => {
+  res.render("galeria")
+});
+
+app.get('/ingressos', (req, res) => {
+  res.render("ingressos")
+});
+
+app.get('/login', (req, res) => {
+  res.render("login")
+});
+
+app.get('/signup', (req, res) => {
+  res.render("signup")
+});
+
+
 app.use('/api/clientes', clientesRouter);
 // app.use('/api/funcionarios', funcionariosRouter);
 // app.use('/api/ingressos', ingressosRouter);
